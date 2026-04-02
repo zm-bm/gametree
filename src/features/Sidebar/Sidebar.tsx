c524a2ee16f21c117e2bf7da4560e821bbeab237
Eric VanderHelm <vanderhelm.eric@gmail.com>
2026-04-02 18:05:08 -0500

    test(sidepane): basic test

--- src/features/Sidebar/Sidebar.tsx
+++ src/features/Sidebar/Sidebar.tsx
@@ -1,17 +1,12 @@
 import ChessBoard from './components/ChessBoard'
 import EngineView from './components/EngineView'
 import OpeningView from './components/OpeningView';
-import { SidebarCard } from './components/SidebarCard';
 
 const Sidebar = () => {
   return (
     <div className="flex-1 flex flex-col space-y-5 h-full">
       <ChessBoard className="sidebar-card" />
-
-      <SidebarCard title="Opening" persistKey='gtOpeningViewCollapsed'>
-        <OpeningView />
-      </SidebarCard>
-
+      <OpeningView />
       <EngineView />
     </div>
   );
